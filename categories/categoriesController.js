1c62066c1e57ff7bd8656849f7b9eeaa23cb6a16
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2024-01-16 19:38:55 -0300

    configurando e adcionado etapas de confirmação de exlusão

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -24,7 +24,28 @@ router.post("/categories/save", (req, res) => {
 });
 
 router.get("/admin/categories", (req, res)=>{
-    res.render("admin/categories/index")
+    Category.findAll().then(categories =>{
+        res.render("admin/categories/index", {categories:categories})
+    })
+})
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id
+    if (id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({
+                where:{
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories")
+            })
+        }else{
+            res.redirect("/admin/categories")
+        }
+    }else{ // NULL
+        res.redirect("/admin/categories");
+    }
 })
 
 module.exports =  router
